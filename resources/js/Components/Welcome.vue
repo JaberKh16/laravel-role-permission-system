53194ba79f87ddf02527e9be6097c0cea44dfb4a
Md. Jaber Khan <md.jaberkhan66@gmail.com>
2022-12-28 11:18:37 +0600

    laravel-inertia-assement-task-2

--- resources/js/Components/Welcome.vue
+++ resources/js/Components/Welcome.vue
@@ -8,39 +8,58 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
         </div>
 
-        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
-            <div v-if="$page.props.user.permissions.includes('View Roles')">
-                <b-button type="button">View Roles</b-button>
+        <div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 pb-3" >
+            <div v-if="$page.props.user.permissions.includes('View Roles')" class="pt-2">
+                <!-- <b-button :type="type">View Roles</b-button> -->
+                <button :type="type" class="inline-flex items-center px-4 py-2 pt-4 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> View Roles
+                </button>
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Create Roles')">
-                <vbutton type="button">Write Roles</vbutton>
+
+            <div v-if="$page.props.user.permissions.includes('Create Roles')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 pt-4 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Create Roles
+                </button>
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Edit Roles')">
-                <b-button type="button" variant="danger">Edit Roles</b-button>
+            <div v-if="$page.props.user.permissions.includes('Edit Roles')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 pt-4 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Edit Roles
+                </button>
 
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Delete Roles')">
-                <b-button type="button">Delete Roles</b-button>
+            <div v-if="$page.props.user.permissions.includes('Delete Roles')" class="pt-2 pb-5">
+                <button :type="type" class="inline-flex items-center px-4 py-2 pt-4 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Delete Roles
+                </button>
             </div>
             
-            <div v-if="$page.props.user.permissions.includes('View Products')">
-                <b-button type="button">View Products</b-button>
+            
+            <div v-if="$page.props.user.permissions.includes('View Products')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 bg-blue-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> View Products
+                </button>
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Create Products')">
-                <b-button type="button">Write Products</b-button>
+            <div v-if="$page.props.user.permissions.includes('Create Products')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Create Products
+                </button>
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Edit Products')">
-                <b-button type="button" variant="danger">Edit Products</b-button>
+            <div v-if="$page.props.user.permissions.includes('Edit Products')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Edit Products
+                </button>
 
             </div>
 
-            <div v-if="$page.props.user.permissions.includes('Delete Products')">
-                <b-button type="button">Delete Products</b-button>
+            <div v-if="$page.props.user.permissions.includes('Delete Products')" class="pt-2">
+                <button :type="type" class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                <slot /> Delete Products
+                </button>
             </div>
         </div>
     </div>
